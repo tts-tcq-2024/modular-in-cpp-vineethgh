f9c5fa3f20f668dcc5b67b1600f435e42cb7c2e6
vineethgh <ghavaladvineeth@gmail.com>
2024-10-03 14:21:02 +0530

    Update conversionFuctions.cpp

--- conversionFuctions.cpp
+++ conversionFuctions.cpp
@@ -2,17 +2,11 @@
 
 namespace TelCoColorCoder 
 {
-
     ColorPair GetColorFromPairNumber(int pairNumber) {
-
 		int zeroBasedPairNumber = pairNumber - 1;
-
 		MajorColor majorColor = static_cast<MajorColor>(zeroBasedPairNumber / numberOfMinorColors);
-
 		MinorColor minorColor = static_cast<MinorColor>(zeroBasedPairNumber % numberOfMinorColors);
-
 		return ColorPair(majorColor, minorColor);
-
     }
 
 	int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
